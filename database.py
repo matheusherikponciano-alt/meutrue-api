9da9b53306f2df7e9a09291a9716280b994acabb
Matheus Herik <matheusherikponciano@gmail.com>
2026-07-26 20:53:19 -0300

    Prepara sincronização com banco da AMT

--- database.py
+++ database.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def conectar():
+    print("HOST:", os.getenv("DB_HOST"))
+    print("PORT:", os.getenv("DB_PORT"))
+    print("USER:", os.getenv("DB_USER"))
+    print("DB:", os.getenv("DB_NAME"))
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         port=int(os.getenv("DB_PORT")),
